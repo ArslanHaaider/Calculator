062bbdc87758ce1d3897e9fb6554b456e1371024
AslanHaaider <aslanhaider707@gmail.com>
2023-05-01 14:45:13 +0500

    Add backspace and clear functionality.

--- script.js
+++ script.js
@@ -53,3 +53,38 @@ equals.addEventListener("click", () => {
         default:
     }
 })
+
+//Clear button event listener
+
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    displayScreen.textContent = "Lets do some Math!"
+    firstNumber = ""
+    secondNumber = ""
+    operator = ""
+    oldText = ""
+})
+
+
+
+//Backspace button event listener
+
+let backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", () => {
+    if(secondNumber.length>0){
+        secondNumber = secondNumber.slice(0, -1);
+        displayScreen.textContent = oldText + secondNumber;
+    }
+    else if(operator.length>0){
+        operator = "";
+        oldText = oldText.slice(0, -1);
+        displayScreen.textContent = oldText;
+    }
+    else if(firstNumber.length>0){
+        firstNumber = firstNumber.slice(0, -1);
+        displayScreen.textContent = firstNumber;
+    }
+    else if(displayScreen.textContent.length==0){
+        displayScreen.textContent = "Lets do some Math!"
+    }
+})
